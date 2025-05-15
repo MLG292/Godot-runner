186b65e1f47c5aba742d88486c1dffaf0114358c
MLG292 <121579346+MLG292@users.noreply.github.com>
2025-05-15 10:22:08 -0400

    Add files via upload

---
+++ Godot/penguin_sledding_simulator_resources/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var friction: = 200
+@export var air_friction: = 60
+@export var acceleration: = 150
+@export var max_speed: = 290
+@export var up_gravity: = 250
+@export var down_gravity: = 500
+@export var jump_force: = 180
+@export var unjump_force: = 25
+
+func _physics_process(delta: float) -> void:	
+	if is_on_floor():
+		if velocity.x < max_speed:
+			velocity.x = move_toward(velocity.x, max_speed, acceleration * delta)
+		else:
+			velocity.x = move_toward(velocity.x, max_speed, friction * delta)
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = -jump_force
+	else:
+		velocity.x = move_toward(velocity.x, 0, air_friction * delta)
+		if Input.is_action_just_released("ui_up"):
+			velocity.y = unjump_force
+		if velocity.y > 0:
+			velocity.y += down_gravity * delta
+		else:
+			velocity.y += up_gravity * delta
+		
+	var previous_velocity = velocity
+	move_and_slide()
+	if velocity.y == 0 and previous_velocity.y > 5:
+		velocity.x += unjump_force * 1.5
+
+	pass
